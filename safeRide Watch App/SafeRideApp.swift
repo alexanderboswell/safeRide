c037345558f5ce5851a05391f138a4ca380f9ab7
Alexander Boswell <alboswel@microsoft.com>
2023-02-15 20:04:01 -0800

    blank project setup

---
+++ safeRide Watch App/SafeRideApp.swift
@@ -0,0 +1,16 @@
+//
+//  SafeRideApp.swift
+//
+//  Created by Alexander Boswell on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SafeRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
